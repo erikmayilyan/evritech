64739a55b08138db79b3c86adda467476534bc0c
Erik <mayilyane9@gmail.com>
2024-11-11 19:15:48 +0100

    nice

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,12 @@ const dbConfig = require('./config');
 
 app.use(express.json());
 
-const frontendUrl = 'https://www.evritech.ca';
+const frontendUrl = process.env.NODE_ENV === 'production' ? 'https://www.evritech.ca' : 'http://localhost:3000';
 
 app.use(cors({
   origin: frontendUrl,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 
