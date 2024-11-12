7428eff97906598b56c16bfecfdc8d5756c0e151
Erik <mayilyane9@gmail.com>
2024-11-12 20:21:42 +0100

    add osme

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const dbConfig = require('./config');
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://www.evritech.ca',
   credentials: true
 }));
 
