0dca591ee4bb92cfce86e7812e0df91d92cc4860
Erik <mayilyane9@gmail.com>
2024-11-13 17:23:43 +0100

    lets see

--- backend/server.js
+++ backend/server.js
@@ -39,12 +39,7 @@ if (process.env.NODE_ENV === 'production') {
 
   app.use(express.static(buildPath));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(buildPath, 'index.html'), function (err) {
-      if (err) {
-        console.error("Error serving index.html:", err);
-        res.status(500).send("Error loading the application.");
-      }
-    });
+    res.sendFile(path.join(buildPath, 'index.html'));
   });
 }
 
